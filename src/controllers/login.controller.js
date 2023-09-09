38b324884b2ab7de815449503e97a41ee2074fc8
ChristianChs <christianchoques@gmail.com>
2023-09-09 13:18:54 -0500

    API Login 2

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,20 +1,40 @@
 import model from "../models/login.model.js";
 
 
-export const login = async(req,res)=>{
-    const { user, password } = req.body;
+export const login = async (req, res) => {
+    const { dni, password } = req.body;
     try {
-      const userFound = await model.findUser(user)
-      console.log(userFound)
-      if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" })
-      const isMatch = password=== userFound[0].clave? true:false
-      if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" })
-      res.json({
-        ruc: userFound[0].ruc,
-        dni: userFound[0].dni,
-        usuario: userFound[0].usuario
-      })
+        const userFound = await model.findUser(dni)
+        console.log(userFound)
+        if (!userFound) return res.status(400).json({ message: "DNI no encontrado" })
+        const isMatch = password === userFound[0].clave ? true : false
+        if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" })
+        res.json({
+            ruc: userFound[0].ruc,
+            dni: userFound[0].dni,
+            usuario: userFound[0].usuario
+        })
     } catch (error) {
-      res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const login2 = async (req, res) => {
+    const { ruc, user, password } = req.body;
+    try {
+        const userFound = await model.findUser2(ruc)
+        console.log(userFound)
+        if (!userFound) return res.status(400).json({ message: "RUC no registrado" })
+        const isMatchUser = user === userFound[0].usuario ? true : false
+        if (!isMatchUser) return res.status(400).json({ message: "Usuario incorrecto" })
+        const isMatch = password === userFound[0].clave ? true : false
+        if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" })
+        res.json({
+            ruc: userFound[0].ruc,
+            dni: userFound[0].dni,
+            usuario: userFound[0].usuario
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
--- src/models/login.model.js
+++ src/models/login.model.js
@@ -3,6 +3,17 @@ import sequelize from "../db.js"
 const model ={}
 
 model.findUser=async(arg)=>{
+    return sequelize.query(`SELECT * FROM login where dni='${arg}'`,{raw:true})
+    .then(([result,metadata])=>{
+        const data= result.length===0?null:result
+        return data
+    })
+    .catch((error)=>{
+        throw error
+    })
+}
+
+model.findUser2=async(arg)=>{
     return sequelize.query(`SELECT * FROM login where ruc='${arg}'`,{raw:true})
     .then(([result,metadata])=>{
         const data= result.length===0?null:result
