da3ce59fb4e8902dc6a747455e00c0015728a424
ChristianChs <christianchoques@gmail.com>
2023-09-09 13:51:12 -0500

    Credentials CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from "express"
 import morgan from "morgan"
+import cors from 'cors'
 import loginRoutes from "./routes/login.route.js"
 
 const app = express()
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
 
 app.use(morgan('dev'))
 
