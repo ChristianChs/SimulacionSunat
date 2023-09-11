fd3e1e786eed5f365af0158c2c05a778f17cb101
oscarchoq <oscarch0qu3@gmail.com>
2023-09-10 21:58:20 -0500

    login correction and implementation

--- cliente/src/api/login.js
+++ cliente/src/api/login.js
@@ -4,4 +4,4 @@ const API = 'http://localhost:3000/api'
 
 export const loginRequest = (user) => axios.post(`${API}/login`, user)
 
-export const loginRequest2 = (user) => axios.post(`${API}/login`, user)
+export const loginRequest2 = (user) => axios.post(`${API}/login2`, user)
