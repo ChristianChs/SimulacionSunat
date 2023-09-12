728d4ee62cbbc05800a61f038754ce246a8099f0
slinareslie <slinareslie@unjbg.edu.pe>
2023-09-12 06:33:02 -0500

    arreglar limite de fechas en credito

--- cliente/src/pages/LoginPage.jsx
+++ cliente/src/pages/LoginPage.jsx
@@ -19,25 +19,34 @@ export default function LoginPage() {
       <div className='mx-auto max-w-screen-lg'>
         <div className='flex items-center justify-center h-screen'>
           <div className='w-full text-center self-center py-5'>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+                {
+                        LoginErrors.map((error, i) => (
+                          <div className='alert alert-danger text-center w-2/5 py-2 px-3 border border-red-300 bg-red-600 rounded-md mb-2 font-sans font-semibold text-white focus:border-yellow-100' key={i}>
+                            {error}
+                          </div>
+                        ))
+                      }
+              </div>
             <div className='py-5 pt-10 sm:pt-2 text-center'>
+              
               <h6 className="mb-0 pb-3">
                 <span className="mr-2 font-sans font-extrabold">Búsqueda por DNI</span>
                 <span className="font-sans font-extrabold">Búsqueda por RUC</span>
               </h6>
               <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
               <label htmlFor="reg-log"></label>
+
+              
               <div className="card-3d-wrap mx-auto">
                 <div className="card-3d-wrapper">
                   <div className='card-front'>
 
                     <div className="center-wrap">
-                      {
-                        LoginErrors.map((error, i) => (
-                          <div className='bg-red-400 text-white' key={i}>
-                            {error}
-                          </div>
-                        ))
-                      }
                       <LoginDNI signin={signin} />
                     </div>
 
@@ -45,13 +54,7 @@ export default function LoginPage() {
                   <div className="card-back">
                     
                     <div className="center-wrap">
-                      {
-                        LoginErrors.map((error, i) => (
-                          <div className='bg-red-400 text-white' key={i}>
-                            {error}
-                          </div>
-                        ))
-                      }
+                      
                       <LoginRUC signinRUC={signinRUC}/>
                     </div>
 
