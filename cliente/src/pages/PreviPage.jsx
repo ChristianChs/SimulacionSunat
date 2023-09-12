956f9fb4b63c9abf4f4bf98f6bc8b4207666ae2c
slinareslie <slinareslie@unjbg.edu.pe>
2023-09-11 19:46:24 -0500

    Cambios innecesarios a preview

--- cliente/src/pages/PreviPage.jsx
+++ cliente/src/pages/PreviPage.jsx
@@ -1,50 +1,97 @@
 import React from 'react';
 import ExamplePDF from './ExamplePDF'
+import ExamplePreview from './ExamplePreview';
 
 function PreviPage() {
     return(
-    <div class="bg-white txt-black border-4 border-white">
-      <div class="container mx-auto p-4 txt-black">
-        <div class="bg-white rounded-md p-4 shadow-md text-stone-950">
-          <h1 class="text-2xl font-bold text-stone-950">ALEXIS DANIEL LIMACHE VILLEGAS</h1>
-          <span class="block">CONO SUR MZ.C LT.40 SHA NEA</span>
-          <ul class="list-none pl-0">
-            <li><strong>Teléfono:</strong> XXXXX</li>
-            <li><strong>Recibi de :</strong> XXXXX</li>
-            <li><strong>Identificado con RUC:</strong> XXXXX</li>
-            <li><strong>Domiciliado en:</strong> XXXXX</li>
-            <li><strong>Tipo de Transacción:</strong> XXXXX</li>
-            <li><strong>La suma de :</strong> XXXXX</li>
-            <li><strong>Por concepto:</strong> XXXXX</li>
-            <li><strong>Inciso:</strong> XXXXX</li>
-            <li><strong>Fecha de Emisión:</strong> XXXXX</li>
-          </ul>
+    <div class="bg-white txt-black border-4 border-white text-black">
+      <div className="container mx-auto mt-4">
+      <div className="flex uppercase">
+        <div className="flex flex-col">
+          <p className="font-bold text-xl">FLORES TICONA ALEX JAVIER</p>
+          <p className="text-sm">MZA. H LOTE. 06 A.V. LA PRADERA III ET TACNA - TACNA - CRL. GREG. ALBARRACIN LANCHIPA</p>
+          <p className="text-sm">TELÉFONO</p>
         </div>
-
-        <div class="fixed top-10  right-16 text-stone-950">
-          <div class="border border-black p-4 w-full">
-            <div class="text-center">
-              <p><strong>RUC del Emisor:</strong> XXXXX</p>
-              <p><strong>RECIBO POR HONORARIOS ELECTRONICO</strong></p>
-            </div>
+        <div className="border border-slate-900 w-[350px] justify-center items-center flex flex-col">
+          <div className="flex justify-center">
+            <p className="text-sm">R.U.C.&nbsp;</p>
+            <p className="text-sm">10758544821</p>
           </div>
+          <p className="text-sm text-center">RECIBO POR HONORARIOS ELECTRONICO</p>
+          <p className="text-sm text-center">Nro: E001-37</p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Recibí de:&nbsp;</p>
+          <p className="uppercase">EULER SMART CLASS S.A.C</p>
         </div>
 
-        <div class="mt-4 text-stone-950">
-          <ul class="list-none pl-0">
-            <center>
-              <li><strong>Total por Honorarios:</strong> XXXXX</li>
-              <li><strong>Retención (8%) IR:</strong> XXXXX</li>
-              <li><strong>Total Neto Recibido:</strong> XXXXX</li>
-            </center>
-          </ul>
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Identificado con&nbsp;</p>
+          <p className="uppercase">RUC</p>
+          <p className="font-bold">&nbsp;número&nbsp;</p>
+          <p className="uppercase">20607257664</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Domiciliado en&nbsp;</p>
+          <p className="uppercase">CAL. SIMON BOLIVAR NRO. 503 TACNA - TACNA - TACNA</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Forma de pago:&nbsp;</p>
+          <p className="uppercase">AL CONTADO</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">La suma de:&nbsp;</p>
+          <p className="uppercase">QUINIENTOS SETENTA Y 00/100 SOLES</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Por concepto&nbsp;</p>
+          <p className="uppercase">DICTADO DE CLASES DE QUIMICA</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Observación&nbsp;</p>
+          <p className="uppercase">-</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Inciso&nbsp;</p>
+          <p className="uppercase">"A"</p>
+          <p className="font-bold uppercase">&nbsp;DEL ARTÍCULO 33 DE LA LEY DEL IMPUESTO A LA RENTA</p>
+        </div>
+
+        <div className="text-sm flex mb-2">
+          <p className="font-bold">Fecha de emisión&nbsp;</p>
+          <p>11 de Septiembre de 2023</p>
+        </div>
       </div>
+
+      <div className="mt-10 text-sm flex flex-col justify-center items-center">
+        <div className="grid grid-cols-2">
+          <p className="font-bold mr-2">Total por honorarios:</p>
+          <p className="font-bold">570.00</p>
+        </div>
+        <div className="grid grid-cols-2">
+          <p className="font-bold mr-2">Retención (8 %) IR:</p>
+          <p className="font-bold">0.00</p>
+        </div>
+        <div className="grid grid-cols-2">
+          <p className="font-bold mr-2">Total Neto Recibido: </p>
+          <p>570.00 SOLES</p>
+        </div>
+      </div>
+      </div>
+      <center>
+        <ExamplePreview/>
+        <ExamplePDF/>
+        <button className='bg-red-900 hover:bg-red-500 text-white font-bold p-2'>Cancelar</button>
+      </center>
     </div>
-    <center>
-      <ExamplePDF/>
-      <button className='bg-red-900 hover:bg-red-500 text-white font-bold p-2'>Cancelar</button>
-    </center>
-  </div>
 
     );
 }
